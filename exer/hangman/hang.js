b937afd1ded4a0252d4d2bd887941e016503de4b
JavaScript <javascript@Admins-iMac-9.local>
2020-01-14 20:50:59 +0200

    14.01.2020

--- exer/hangman/hang.js
+++ exer/hangman/hang.js
@@ -1,5 +1,5 @@
 var game = {
-    word: ['apple', 'orange' , 'bana'],
+    words: ['apple', 'orange' , 'banana'],
     word: undefined,
     letters: [],
     triestLeft: 6,
@@ -7,8 +7,8 @@ var game = {
         this.word = this.randomWord();
     },
     randomWord: function() {
-        let index = Math.floor(Math.random() * this.words.lenght);
-        return this.word[index];
+        let index = Math.floor(Math.random() * this.words.length);  
+        return this.words[index];
     },
     tryLetter: function(letter) {
         this.letters.push(letter);
@@ -20,6 +20,37 @@ var game = {
         return this.triestLeft == 0;
     },
     isWinner: function() {
-    
+        return this.word.split('')
+                   .every(letter => this.letters.includes(letter))
     }
 };
+
+function renderLetters() {
+    document.getElementById("letters").innerText = game.letters.join(' ');
+}
+function renderWord() {
+    let text = game.word.split('')
+                        .map(letter => game.letters.includes(letter) ? letter : '_')
+                        .join(' ');
+    document.getElementById("word").innerText = text;
+
+}
+function renderDrawing() {
+    let parts = ['head', 'body', 'left_arm', 'right_arm', 'left_leg', 'right_leg'];
+    let visible = parts.slice(0 , 6 - game.triestLeft);
+    let invisible = parts.slice(6 - game.triestLeft);
+    visible.forEach(name => document.getElementById(name).style.visibility = 'visible');
+}
+function renderView() {
+    renderLetters();
+    renderWord();
+    renderDrawing();   
+}
+window.addEventListener('keydown', function(event) {
+    if (game.isDead() || game.isWinner()) return;
+    game.tryLetter(event.key);
+    renderView();
+});
+
+game.start();
+renderView();
